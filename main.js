05ef5124e6eb24af28ec8ab2a022466192ac9ad0
Ben Wheatley <BenWheatley@users.noreply.github.com>
2025-07-10 09:59:40 +0200

    created main.js manually based on main.cpp

---
+++ main.js
@@ -0,0 +1,17 @@
+// This one file was written by Ben, not AI, examining main.cpp and replacing it with what seems to makes sense for a JS webapp (i.e. it's not starting off by loading any files)
+
+function main() {
+	console.log(
+		"Original code by Bungie Software <http://www.bungie.com/>\n" +
+		"Additional work by Loren Petrich, Chris Pruett, Rhys Hill et al.\n" +
+		"TCP/IP networking by Woody Zenfell\n" +
+		"SDL port by Christian Bauer <Christian.Bauer@uni-mainz.de>\n" +
+		"Mac OS X/SDL version by Chris Lovell, Alexander Strange, and Woody Zenfell\n" +
+		"JS rewrite, Ben Wheatley\n" +
+		"\nThis is free software with ABSOLUTELY NO WARRANTY.\n" +
+		"You are welcome to redistribute it under certain conditions.\n" +
+		"For details, see the file COPYING.\n"
+	);
+	initialize_application();
+	main_event_loop();
+}
