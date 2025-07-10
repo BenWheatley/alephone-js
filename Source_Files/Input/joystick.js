88680c109ad7e27d53f68b3b00b0989812977e88
Ben Wheatley <BenWheatley@users.noreply.github.com>
2025-07-10 21:16:42 +0200

    block out with comment

--- Source_Files/Input/joystick.js
+++ Source_Files/Input/joystick.js
@@ -38,6 +38,7 @@ const AO_SCANCODE_JOYSTICK_ESCAPE = AO_SCANCODE_BASE_JOYSTICK_BUTTON + SDL_CONTR
 /*
 JOYSTICK_SDL.CPP
 */
+/*
 #include "player.h" // for mask_in_absolute_positioning_information
 #include "preferences.h"
 #include "joystick.h"
@@ -224,3 +225,4 @@ int process_joystick_axes(int flags) {
 		flags = process_aim_input(flags, {dyaw, dpitch});
 	return flags;
 }
+*/
