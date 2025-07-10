f56e1ac5cfaf122edecdc77bec43a8ff763403dc
Ben Wheatley <BenWheatley@users.noreply.github.com>
2025-07-10 23:20:33 +0200

    simplify use of Logging

--- Source_Files/Misc/Logging.js
+++ Source_Files/Misc/Logging.js
@@ -24,20 +24,20 @@
 	May 21, 2003 (Woody Zenfell): being a little more defensive about NULL file pointer.
 */
 
-export const LogLevel = Object.freeze({
-	logFatalLevel: 0,     // program must exit
-	logErrorLevel: 10,    // can't do something significant
-	logWarningLevel: 20,  // can continue but results could be really screwy
-	logAnomalyLevel: 30,  // can continue, results could be off a little, but no big deal
-	logNoteLevel: 40,     // something worth mentioning
-	logSummaryLevel: 45,  // occasional dumps of aggregate statistics
-	logTraceLevel: 50,    // details of actions and logic
-	logDumpLevel: 60      // values of data etc.
+export const Level = Object.freeze({
+	fatal: 0,     // program must exit
+	error: 10,    // can't do something significant
+	warning: 20,  // can continue but results could be really screwy
+	anomaly: 30,  // can continue, results could be off a little, but no big deal
+	note: 40,     // something worth mentioning
+	summary: 45,  // occasional dumps of aggregate statistics
+	trace: 50,    // details of actions and logic
+	dump: 60      // values of data etc.
 });
 
 // JS conversion note: change of syntax from GetCurrentLogger().foo() to Logging.foo()
 
-export let loggingThreshhold = LogLevel.logNoteLevel; // log messages at or above this level will be squelched // TODO: fix mis-spelling once the app actually works
+export let loggingThreshhold = Level.logNoteLevel; // log messages at or above this level will be squelched // TODO: fix mis-spelling once the app actually works
 
 export function logMessage(level, file, line, message, ...args) {
 	// TODO: rm `file`, `line` as not sensible in JS land, but only after app actually runs
