68f3b1e40e72000e21781ccdde3ee1c0a7e2b255
Ben Wheatley <BenWheatley@users.noreply.github.com>
2025-07-13 01:56:15 +0200

    convert cspixels.h to .js

--- Source_Files/CSeries/cspixels.js
+++ Source_Files/CSeries/cspixels.js
@@ -1,5 +1,4 @@
 /*
-
 	Copyright (C) 1991-2001 and beyond by Bo Lindbergh
 	and the "Aleph One" developers.
  
@@ -16,23 +15,15 @@
 	This license is contained in the file "COPYING",
 	which is included with this source code; it is available online at
 	http://www.gnu.org/licenses/gpl.html
-
 */
-
-#ifndef _CSERIES_PIXELS_
-#define _CSERIES_PIXELS_
 
 // Need this here
 #include "cstypes.h"
 
-typedef uint8 pixel8;
-typedef uint16 pixel16;
-typedef uint32 pixel32;
-
-#define PIXEL8_MAXIMUM_COLORS 256
-#define PIXEL16_MAXIMUM_COMPONENT 31
-#define PIXEL32_MAXIMUM_COMPONENT 255
-#define NUMBER_OF_COLOR_COMPONENTS 3
+export const PIXEL8_MAXIMUM_COLORS = 256;
+export const PIXEL16_MAXIMUM_COMPONENT = 31;
+export const PIXEL32_MAXIMUM_COMPONENT = 255;
+export const NUMBER_OF_COLOR_COMPONENTS = 3;
 
 /*
 	note that the combiner macros expect input values in the range
@@ -41,15 +32,35 @@ typedef uint32 pixel32;
 		0x00 through 0x1F (in the 16-bit case)
 		0x00 through 0xFF (in the 32-bit case)
  */
+ 
+function RGBCOLOR_TO_PIXEL16(r, g, b) {
+	return ((r >> 1) & 0x7C00) | ((g >> 6) & 0x03E0) | ((b >> 11) & 0x001F);
+}
 
-#define RGBCOLOR_TO_PIXEL16(r,g,b) (((r)>>1&0x7C00) | ((g)>>6&0x03E0) | ((b)>>11&0x001F))
-#define RED16(p) ((p)>>10&0x1F)
-#define GREEN16(p) ((p)>>5&0x1F)
-#define BLUE16(p) ((p)&0x1F)
+function RED16(p) {
+    return (p >> 10) & 0x1F;
+}
 
-#define RGBCOLOR_TO_PIXEL32(r,g,b) (((r)<<8&0x00FF0000) | ((g)&0x00000FF00) | ((b)>>8&0x000000FF))
-#define RED32(p) ((p)>>16&0xFF)
-#define GREEN32(p) ((p)>>8&0xFF)
-#define BLUE32(p) ((p)&0xFF)
+function GREEN16(p) {
+    return (p >> 5) & 0x1F;
+}
 
-#endif
+function BLUE16(p) {
+    return p & 0x1F;
+}
+
+function RGBCOLOR_TO_PIXEL32(r, g, b) {
+    return ((r << 8) & 0x00FF0000) | ((g) & 0x00000FF00) | ((b >> 8) & 0x000000FF);
+}
+
+function RED32(p) {
+    return (p >> 16) & 0xFF;
+}
+
+function GREEN32(p) {
+    return (p >> 8) & 0xFF;
+}
+
+function BLUE32(p) {
+    return p & 0xFF;
+}
