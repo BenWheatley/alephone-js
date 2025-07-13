19f02634aa6ce939ed67d7347abf390315702cb8
Ben Wheatley <BenWheatley@users.noreply.github.com>
2025-07-13 23:13:42 +0200

    stubbed out file for Music.js, usual approach

--- Source_Files/Sound/Music.js
+++ Source_Files/Sound/Music.js
@@ -1,5 +1,4 @@
 /*
-
 	Copyright (C) 1991-2001 and beyond by Bungie Studios, Inc.
 	and the "Aleph One" developers.
  
@@ -17,9 +16,98 @@
 	which is included with this source code; it is available online at
 	http://www.gnu.org/licenses/gpl.html
 
+	Handles both intro and level music
 */
 
-#include "Music.h"
+import * as cseries from '../CSeries/cseries.js';
+
+/*
+#include "Decoder.h"
+#include "FileHandler.h"
+#include "Random.h"
+#include "SoundManager.h"
+#include "MusicPlayer.h"
+
+class Music
+{
+public:
+	static Music *instance() { 
+		static Music *m_instance = nullptr;
+		if (!m_instance) 
+			m_instance = new Music(); 
+		return m_instance; 
+	}
+
+	static constexpr int reserved_music_slots = 2;
+	enum MusicSlot {
+		Intro = 0,
+		Level = 1
+	};
+
+	bool SetupIntroMusic(FileSpecifier &file) { return music_slots[MusicSlot::Intro].Open(&file); }
+	void RestartIntroMusic();
+
+	void Fade(float limitVolume, short duration, bool stopOnNoVolume = true, int index = NONE);
+	void Pause(int index = NONE);
+	bool Playing(int index = NONE);
+	int Load(FileSpecifier& file, bool loop, float volume);
+	void Play(int index) { music_slots[index].Play(); }
+	void Idle();
+	bool IsInit(int index) const { return music_slots.size() > index && music_slots[index].IsInit(); }
+	float GetVolume(int index) const { return music_slots[index].GetVolume(); }
+	void SetVolume(int index, float volume) { music_slots[index].SetVolume(volume); }
+	void StopLevelMusic() { music_slots[MusicSlot::Level].Close(); }
+	void StopInGameMusic();
+	void ClearLevelMusic();
+	void PushBackLevelMusic(const FileSpecifier& file);
+	void LevelMusicRandom(bool fRandom) { random_order = fRandom; }
+	void SeedLevelMusic();
+	void SetClassicLevelMusic(short song_index);
+	bool HasClassicLevelMusic() const { return marathon_1_song_index >= 0; }
+private:
+	class Slot {
+	private:
+		std::shared_ptr<MusicPlayer> musicPlayer;
+		std::shared_ptr<StreamDecoder> decoder;
+		FileSpecifier music_file;
+		uint64_t music_fade_start = 0;
+		uint32 music_fade_duration = 0;
+		float music_fade_limit_volume;
+		float music_fade_start_volume;
+		bool music_fade_stop_no_volume;
+		MusicParameters parameters;
+	public:
+		void Fade(float limitVolume, short duration, bool stopOnNoVolume = true);
+		bool Playing() const { return IsInit() && musicPlayer && musicPlayer->IsActive(); }
+		bool Open(FileSpecifier* file);
+		void Pause();
+		void Close();
+		bool SetParameters(bool loop, float volume);
+		void Play();
+		float GetLimitFadeVolume() const { return music_fade_limit_volume; }
+		bool IsInit() const { return decoder != nullptr; }
+		bool IsFading() const { return music_fade_start; }
+		bool StopPlayerAfterFadeOut() const { return music_fade_stop_no_volume; }
+		void StopFade() { music_fade_start = 0; }
+		void SetVolume(float volume);
+		float GetVolume() const { return parameters.volume; }
+		std::pair<bool, float> ComputeFadingVolume() const;
+	};
+
+	std::vector<Slot> music_slots;
+
+	Music();
+	FileSpecifier* GetLevelMusic();
+	bool LoadLevelMusic();
+
+	// level music
+	short marathon_1_song_index;
+	std::vector<FileSpecifier> playlist;
+	size_t song_number;
+	bool random_order;
+	GM_Random randomizer;
+};
+
 #include "interface.h"
 #include "OpenALManager.h"
 
@@ -262,3 +350,4 @@ FileSpecifier* Music::GetLevelMusic()
 
 	return &playlist[song_number++];
 }
+*/
